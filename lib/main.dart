56d5b3e010849faf7aca59a2e165bb9c94e1cacf
3lVv0w <krseisenh@gmail.com>
2020-12-02 20:32:05 +0700

    update build gradle

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,28 @@
+import 'dart:async';
+
+import 'package:firebase_analytics/firebase_analytics.dart';
+import 'package:firebase_analytics/observer.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:flutter/material.dart';
 import 'package:scoped_model/scoped_model.dart';
 import 'package:tongmoopa/utlity/scoped_models/app_model.dart';
 import 'package:tongmoopa/widget/authen.dart';
 
-main() => runApp(MyApp());
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runZonedGuarded(() {
+    runApp(MyApp());
+  }, (error, stackTrace) {
+    FirebaseCrashlytics.instance.recordError(error, stackTrace);
+  });
+}
 
 class MyApp extends StatelessWidget {
+  static FirebaseAnalytics analytics = FirebaseAnalytics();
+  static FirebaseAnalyticsObserver observer =
+      FirebaseAnalyticsObserver(analytics: analytics);
   @override
   Widget build(BuildContext context) {
     final AppModel model = AppModel();
@@ -18,6 +35,7 @@ class MyApp extends StatelessWidget {
         theme: ThemeData(
           primarySwatch: Colors.blue,
         ),
+        navigatorObservers: <NavigatorObserver>[observer],
       ),
     );
   }
